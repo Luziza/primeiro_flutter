e033c407e49c1a539f905553c71786910118f684
Luiza Ribeiro <ribeiroluiza.jlle@gmail.com>
2024-01-02 19:18:16 -0300

    Tela inicial do app de tarefas

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:primeiro/Tarefa.dart';
+import 'package:primeiro/Formulario.dart';
+import 'package:primeiro/data.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
+  get children => null;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
         title: 'Flutter Demo',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-              children: [
-                Container(color: Colors.amber, height: 100, width: 100,),
-                Container(color: Colors.blue, height: 100, width: 100,),
-                Container(color: Colors.deepPurple, height: 100, width: 100,),
-              ]
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-            children: [
-            Container(color: Colors.deepPurple, height: 100, width: 100,),
-            Container(color: Colors.lightGreen, height: 100, width: 100,),
-            Container(color: Colors.pink, height: 100, width: 100,),
-        ]
-    ),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-                children: [
-                  Container(color: Colors.deepOrange, height: 100, width: 100,),
-                  Container(color: Colors.indigo, height: 100, width: 100,),
-                  Container(color: Colors.purpleAccent, height: 100, width: 100,),
-                ]
-            ),
-      ]
-        ),
+        home: TaskInherite(child: const Tarefa())
     );
   }
 }
-
