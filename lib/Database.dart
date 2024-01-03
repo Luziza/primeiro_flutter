d22f6e6e18ab79ab75135d1b40663c419a23db05
Luiza Ribeiro <ribeiroluiza.jlle@gmail.com>
2024-01-03 14:22:16 -0300

    database

---
+++ lib/Database.dart
@@ -0,0 +1,13 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+///Método tipo Future é quando o método precisa se conectar com algo de fora, como pos
+///exemplo um banco da dados e é tipo Database
+Future<Database> getDataBase()
+  async{
+  ///Path é o local no aparelho onde o banco de dados vai ser salvo
+  final String path = join(
+      await getDatabasesPath(), 'task.db');
+      ///O getDatabasesPath procura no dispositivo o caminho perfeito pra salvar arquivos
+  return openDatabase(path)
+}
